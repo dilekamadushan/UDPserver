f7e53fff0e4e6699602a8e44d7bd813f930fa72e
Randil Fernando <randil.fernando@outlook.com>
2018-11-29 17:05:36 +0530

    get ip address automatically

--- src/main/java/com/grydtech/peershare/PeerShareApplication.java
+++ src/main/java/com/grydtech/peershare/PeerShareApplication.java
@@ -7,10 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-import java.net.BindException;
-import java.net.SocketException;
+import java.net.*;
+import java.util.Objects;
 
 @SpringBootApplication
 public class PeerShareApplication {
@@ -20,9 +21,6 @@ public class PeerShareApplication {
 
     @Value("${bootstrap.port}")
     private int bootstrapPort;
-
-    @Value("${server.host}")
-    private String serverHost;
 
     @Value("${server.port}")
     private int httpPort;
@@ -35,15 +33,33 @@ public class PeerShareApplication {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final Server server;
+    private final Environment environment;
 
     @Autowired
-    public PeerShareApplication(SimpMessagingTemplate simpMessagingTemplate, Server server) {
+    public PeerShareApplication(SimpMessagingTemplate simpMessagingTemplate, Server server, Environment environment) {
         this.simpMessagingTemplate = simpMessagingTemplate;
         this.server = server;
+        this.environment = environment;
     }
 
     @EventListener
-    public void afterApplicationReady(ApplicationReadyEvent event) {
+    public void afterApplicationReady(ApplicationReadyEvent event) throws UnknownHostException {
+        String serverHost = environment.getProperty("server.host");
+
+        if (serverHost == null || serverHost.equals("")) {
+            try (final DatagramSocket socket = new DatagramSocket()) {
+                socket.connect(new InetSocketAddress("8.8.8.8", 10002));
+                serverHost = socket.getLocalAddress().getHostAddress().trim();
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if ("0.0.0.0".equals(serverHost) || "127.0.0.1".equals(serverHost)) {
+            InetAddress localhost = InetAddress.getLocalHost();
+            serverHost = localhost.getHostAddress().trim();
+        }
+
         try {
             server.initServer(bootstrapHost, bootstrapPort, serverHost, httpPort + 10000, serverName, kafkaHost,simpMessagingTemplate);
             System.out.println("Starter: started successfully");
